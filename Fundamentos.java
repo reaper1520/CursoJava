7bac007f4ba82f1c474b1248b1523bfdcbffb9d7
Thiago Sobral <57776659+reaper1520@users.noreply.github.com>
2020-08-26 21:07:43 -0300

    Add files via upload

--- Fundamentos.java
+++ Fundamentos.java
@@ -81,10 +81,36 @@ public class Fundamentos {
 			System.out.println("Opção Invádida");
 			break;
 		}
-		
-		
-		
-		
+		System.out.println("");
+		System.out.println("Estruturas de repetição");
+		System.out.println("");
+		System.out.println("Exemplo 5 - Uso do 'for'");
+		for (int j = 10; j > 0; j--) {
+			System.out.println(j);
+		}
+		System.out.println("");
+		System.out.println("Exemplo 6 - Tabuada");
+		for ( int tabuada = 0; tabuada <=10; tabuada++ ) {
+			System.out.println("");
+			for (int valor = 0; valor <=10; valor++) {
+				System.out.println(tabuada + " x " + valor + " = " + (tabuada * valor));
+			}
+		}
+		System.out.println("");
+		System.out.println("Exemplo 7 - Uso da estrutura while");
+		int contador = 1;
+		while (contador <=10) {
+			System.out.println(contador);
+			contador++;
+		}
+		System.out.println("");
+		System.out.println("Exemplo 8 - Uso da estrutura do while");
+		char novoJogo;
+		do {
+			System.out.println("Deseja jogar novamente (S/N)?");
+			novoJogo = 'n';	
+		} while (novoJogo == 's');
+		System.out.println("GAMER OVER");
 	}
 
 }
